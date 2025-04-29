2237aa606b9dba5955c3ac5ed574b56e71294f07
AldoRubenC <35183502+AldoRubenC@users.noreply.github.com>
2025-04-28 19:57:13 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
-from telegram import Update
-from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
 import os
+from telegram import Update
+from telegram.ext import ApplicationBuilder, MessageHandler, ChannelPostHandler, filters, ContextTypes
+import nest_asyncio
 
+# Función para manejar mensajes en grupos o privados
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     chat_id = update.effective_chat.id
-    await update.message.reply_text(f"El ID de este canal/grupo es: {chat_id}")
+    await update.message.reply_text(f"El ID de este grupo o chat privado es: {chat_id}")
+
+# Función para manejar mensajes publicados en canales
+async def handle_channel_post(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    chat_id = update.effective_chat.id
+    await update.channel_post.reply_text(f"El ID de este canal es: {chat_id}")
 
 async def main():
     token = os.getenv("BOT_TOKEN","7714496610:AAG91ImaiK5EKH5Dcn1kjTn6T3w-GRD0Y4o")
     app = ApplicationBuilder().token(token).build()
-    
-    app.add_handler(MessageHandler(filters.ALL, handle_message))
-    
+
+    # Handler para mensajes normales (grupos o privados)
+    app.add_handler(MessageHandler(filters.TEXT & (~filters.COMMAND), handle_message))
+
+    # Handler para mensajes en canales
+    app.add_handler(ChannelPostHandler(handle_channel_post))
+
     await app.run_polling()
 
 if __name__ == '__main__':
