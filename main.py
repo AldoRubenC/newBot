aa46b05159aa2ab35c8b029c6453576cefe65514
AldoRubenC <35183502+AldoRubenC@users.noreply.github.com>
2025-04-28 19:28:42 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler
+from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
+import os
 
-async def get_group_id(update: Update, context):
-    chat_id = update.message.chat.id
-    await update.message.reply_text(f"El ID de este grupo es: {chat_id}")
+async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    chat_id = update.effective_chat.id
+    await update.message.reply_text(f"El ID de este canal/grupo es: {chat_id}")
 
 async def main():
     token = os.getenv("7714496610:AAG91ImaiK5EKH5Dcn1kjTn6T3w-GRD0Y4o")
     app = ApplicationBuilder().token(token).build()
     
-    app.add_handler(CommandHandler("getgroupid", get_group_id))
+    app.add_handler(MessageHandler(filters.ALL, handle_message))
     
     await app.run_polling()
 
 if __name__ == "__main__":
     import asyncio
     asyncio.run(main())
-
